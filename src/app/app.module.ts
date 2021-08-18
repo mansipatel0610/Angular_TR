81651227a048e8e58e3b07c38142aed207c636f9
Mansi Patel <mansipatel63673@gmail.com>
2021-08-18 10:35:39 +0530

    Reactive Forms Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms'; 
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -50,10 +53,13 @@ import { FormsModule } from '@angular/forms';
     MatButtonModule,
     MatListModule,
     AppRoutingModule,
+    MatSelectModule,
+    MatSlideToggleModule,
     MatFormFieldModule, 
     MatInputModule,
     MatCheckboxModule,
     MatDialogModule,
+    ReactiveFormsModule,
     FormsModule 
    
   ],
