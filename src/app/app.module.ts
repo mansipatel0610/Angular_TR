304a13b18a4eb79144989746d72a2a018d021b48
Mansi Patel <mansipatel63673@gmail.com>
2021-08-19 10:30:36 +0530

    Promise part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,7 @@ import { FormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 
 @NgModule({
@@ -60,6 +61,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatCheckboxModule,
     MatDialogModule,
     ReactiveFormsModule,
+    MatProgressSpinnerModule,
     FormsModule 
    
   ],
